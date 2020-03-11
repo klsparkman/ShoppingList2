0deb43a9a3a93e896093eeb1596371900537957d
klsparkman <61604055+klsparkman@users.noreply.github.com>
2020-03-10 19:20:10 -0600

    Add files via upload

---
+++ ShoppingList2/ShoppingList2/Model/ListController.swift
@@ -0,0 +1,19 @@
+//
+//  ListController.swift
+//  ShoppingList2
+//
+//  Created by Kelsey Sparkman on 3/10/20.
+//  Copyright © 2020 Kelsey Sparkman. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+class ListController {
+    
+    static let shared = 
+    
+    
+    
+    
+} //End Of Class
